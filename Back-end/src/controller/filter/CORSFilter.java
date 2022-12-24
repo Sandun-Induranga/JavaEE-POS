f572d497f5584da744142e670f1efe474b5f9938
Sandun-Induranga <sandutharu40@gmail.com>
2022-12-24 10:01:21 +0530

    Added CORS Filter

---
+++ Back-end/src/controller/filter/CORSFilter.java
@@ -0,0 +1,33 @@
+package controller.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+@WebFilter(urlPatterns = "/*")
+public class CORSFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        filterChain.doFilter(servletRequest,servletResponse);
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        resp.addHeader("Access-Control-Allow-Origin","*");
+        resp.addHeader("Access-Control-Allow-Methods","DELETE,PUT");
+        resp.addHeader("Access-Control-Allow-Headers","Content-Type");
+        resp.setContentType("application/json");
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
