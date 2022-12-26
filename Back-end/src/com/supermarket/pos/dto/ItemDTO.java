8558cff71bee33f634fb2115543708df94473e0d
Sandun-Induranga <sandutharu40@gmail.com>
2022-12-26 20:57:56 +0530

    Created Class ItemDTO

--- Back-end/src/com/supermarket/pos/dto/ItemDTO.java
+++ Back-end/src/com/supermarket/pos/dto/ItemDTO.java
@@ -1,8 +1,18 @@
 package com.supermarket.pos.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 /**
  * @author : Sandun Induranga
  * @since : 0.1.0
  **/
+
+@Data
+@AllArgsConstructor
 public class ItemDTO {
+    private String code;
+    private String name;
+    private String qtyOnHand;
+    private double price;
 }
