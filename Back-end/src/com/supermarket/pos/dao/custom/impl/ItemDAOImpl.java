e6b22fab3605147bea7c2d96d7718a0fb8ec1339
Sandun-Induranga <sandutharu40@gmail.com>
2022-12-26 21:34:42 +0530

    ItemDAOImpl getAll() Refactored

--- Back-end/src/com/supermarket/pos/dao/custom/impl/ItemDAOImpl.java
+++ Back-end/src/com/supermarket/pos/dao/custom/impl/ItemDAOImpl.java
@@ -1,9 +1,11 @@
 package com.supermarket.pos.dao.custom.impl;
 
+import com.supermarket.pos.dao.SQLUtil;
 import com.supermarket.pos.dao.custom.ItemDAO;
 import com.supermarket.pos.entity.Item;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -15,7 +17,12 @@ import java.util.ArrayList;
 public class ItemDAOImpl implements ItemDAO {
     @Override
     public ArrayList<Item> getAll(Connection connection) throws SQLException, ClassNotFoundException {
-        return null;
+        ResultSet rst = SQLUtil.executeQuery(connection, "SELECT * FROM Item");
+        ArrayList<Item> allItems = new ArrayList<>();
+        while (rst.next()) {
+            allItems.add(new Item(rst.getString(1), rst.getString(2), rst.getInt(3), rst.getDouble(4)));
+        }
+        return allItems;
     }
 
     @Override
