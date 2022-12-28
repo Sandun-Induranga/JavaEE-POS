a8db738ee0d8ab078f32b705d8d179ed172b7431
Sandun-Induranga <sandutharu40@gmail.com>
2022-12-28 20:43:05 +0530

    Created Class OrderDAOImpl

---
+++ Back-end/src/com/supermarket/pos/dao/custom/impl/OrderDAOImpl.java
@@ -0,0 +1,44 @@
+package com.supermarket.pos.dao.custom.impl;
+
+import com.supermarket.pos.dao.custom.OrderDAO;
+import com.supermarket.pos.entity.Order;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+public class OrderDAOImpl implements OrderDAO {
+    @Override
+    public ArrayList<Order> getAll(Connection connection) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean save(Connection connection, Order dto) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean update(Connection connection, Order dto) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public Order search(Connection connection, String s) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean exit(Connection connection, String s) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(Connection connection, String s) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+}
