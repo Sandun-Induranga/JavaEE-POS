5930bac78c76a0598f779a1b057315ac08e33509
Sandun-Induranga <sandutharu40@gmail.com>
2022-12-28 20:22:15 +0530

    Created Class OrderDTO

---
+++ Back-end/src/com/supermarket/pos/dto/OrderDTO.java
@@ -0,0 +1,17 @@
+package com.supermarket.pos.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+@Data
+@AllArgsConstructor
+public class OrderDTO {
+    private String orderId;
+    private String cusId;
+    private double cost;
+}
