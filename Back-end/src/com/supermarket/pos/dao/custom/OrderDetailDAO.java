c9760da28f507fc8516e861164a628cb3875c8ba
Sandun-Induranga <sandutharu40@gmail.com>
2022-12-29 11:43:18 +0530

    Created Interface OrderdetailDAO

--- Back-end/src/com/supermarket/pos/dao/custom/OrderDetailDAO.java
+++ Back-end/src/com/supermarket/pos/dao/custom/OrderDetailDAO.java
@@ -1,10 +1,14 @@
 package com.supermarket.pos.dao.custom;
 
 import com.supermarket.pos.dao.CrudDAO;
+import com.supermarket.pos.entity.OrderDetail;
+
+import java.sql.Connection;
 
 /**
  * @author : Sandun Induranga
  * @since : 0.1.0
  **/
-public interface OrderDetailDAO extends CrudDAO<> {
+
+public interface OrderDetailDAO extends CrudDAO<Connection, OrderDetail, String> {
 }
