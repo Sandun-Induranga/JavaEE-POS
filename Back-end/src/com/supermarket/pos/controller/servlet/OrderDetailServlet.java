26860f588f0e9381fed2fa20bc2800f00ed7a8e0
Sandun-Induranga <sandutharu40@gmail.com>
2023-01-02 21:45:27 +0530

    Order Details Servlet References Added

--- Back-end/src/com/supermarket/pos/controller/servlet/OrderDetailServlet.java
+++ Back-end/src/com/supermarket/pos/controller/servlet/OrderDetailServlet.java
@@ -1,10 +1,15 @@
 package com.supermarket.pos.controller.servlet;
 
+import com.supermarket.pos.bo.BOFactory;
+import com.supermarket.pos.bo.custom.OrderDetailBO;
+
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.IOException;
 
 /**
@@ -13,6 +18,10 @@ import java.io.IOException;
  **/
 @WebServlet(urlPatterns = "/order_detail")
 public class OrderDetailServlet extends HttpServlet {
+
+    @Resource(name = "java:comp/env/jdbc/pool")
+    private DataSource dataSource;
+    OrderDetailBO orderDetailBO = (OrderDetailBO) BOFactory.getInstance().getBO(BOFactory.BOTypes.ORDER_DETAILS);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
