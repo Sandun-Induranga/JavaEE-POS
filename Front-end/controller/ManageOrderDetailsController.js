d0d1b187a6f06813027fbf32c1c6fce33188082e
Sandun-Induranga <sandutharu40@gmail.com>
2023-01-02 20:59:41 +0530

    Content Updated

--- Front-end/controller/ManageOrderDetailsController.js
+++ Front-end/controller/ManageOrderDetailsController.js
@@ -3,3 +3,13 @@
  * @since : 0.1.0
  **/
 
+function loadAllOrders() {
+    $("#tblOrders > tbody").empty();
+
+    for (let order of ordersDB) {
+        $("#tblOrders > tbody").append(
+            `<tr><td>${order.orderId}</td><td>${order.customerId}</td><td>${order.customerName}</td><td>${order.date}</td><td>${order.total}</td><td>${order.discount}</td><td>${order.amount}</td><td><i class="bi bi-trash text-danger order-deletes"></i></td></tr>`
+        );
+    }
+    bindOrdersDeleteEvent();
+}
