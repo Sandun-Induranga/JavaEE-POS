dd0b87c376cd8e56de4887f7dea2c0a3db4aeeed
Sandun-Induranga <sandutharu40@gmail.com>
2023-01-02 21:29:11 +0530

    Created Interface OrderDetailBO

---
+++ Back-end/src/com/supermarket/pos/bo/custom/OrderDetailBO.java
@@ -0,0 +1,17 @@
+package com.supermarket.pos.bo.custom;
+
+import com.supermarket.pos.bo.SuperBO;
+import com.supermarket.pos.dto.OrderDetailDTO;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+public interface OrderDetailBO extends SuperBO {
+
+    ArrayList<OrderDetailDTO> getAllOrderDetails(String orderID) throws SQLException, ClassNotFoundException;
+
+}
